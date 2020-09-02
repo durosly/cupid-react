de052c0d081be77ebcd9b4120d8ae3c1064a3069
Albert Slyvester Duro <slyboydon1@gmail.com>
2020-09-02 16:34:03 +0100

    added service worker using workbox P.S (strategy used is network first)

---
+++ public/sw.js
@@ -0,0 +1,32 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js')
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`)
+} else {
+  console.log(`Boo! Workbox didn't load 😬`)
+}
+
+workbox.routing.registerRoute(
+    /\.(?:js|css|html|.ttf|.png)$/,
+    new workbox.strategies.NetworkFirst()
+)
+
+workbox.routing.registerRoute(
+    'http://localhost:3000',
+    new workbox.strategies.NetworkFirst()
+)
+
+workbox.routing.registerRoute(
+    new RegExp('/assets/.+'),
+    new workbox.strategies.CacheFirst()
+)
+
+workbox.routing.registerRoute(
+    /https:\/\/fonts.googleapis.com\/.+/,
+    new workbox.strategies.NetworkFirst()
+)
+
+workbox.routing.registerRoute(
+    /https:\/\/firebasestorage.googleapis.com\/v0\/b\/cupid-portfolio.appspot.com\/o\/.+/,
+    new workbox.strategies.NetworkFirst()
+)
